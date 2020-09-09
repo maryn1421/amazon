d9367e755a5b71c0a08ae9886d929abef80c941b
maryn1421 <zolnaimaryn1421@gmail.com>
2020-09-09 12:14:10 +0200

    #2 finished header component, the header of the page

---
+++ my-app/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDf1vdl9EhOPCGvCCgaaVel7THmp6v5btY",
+    authDomain: "challenge-8d911.firebaseapp.com",
+    databaseURL: "https://challenge-8d911.firebaseio.com",
+    projectId: "challenge-8d911",
+    storageBucket: "challenge-8d911.appspot.com",
+    messagingSenderId: "419876125061",
+    appId: "1:419876125061:web:c07b49f70f830981c62cec",
+    measurementId: "G-8S4XJVX983"
+};
